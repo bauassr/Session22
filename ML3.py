790689db194c1889971b96d596c5cb561f451173
bauassr <bauassr@gmail.com>
2018-07-15 22:34:07 +0530

    Add files via upload

---
+++ ML3.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from patsy import dmatrices
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from sklearn.cross_validation import cross_val_score
+dta = sm.datasets.fair.load_pandas().data
+
+# add "affair" column: 1 represents having affairs, 0 represents not
+dta['affair'] = (dta.affairs > 0).astype(int)
+y, X = dmatrices('affair ~ rate_marriage + age + yrs_married + children + \
+religious + educ + C(occupation) + C(occupation_husb)',
+dta, return_type="dataframe")
+
+X = X.rename(columns = {'C(occupation)[T.2.0]':'occ_2',
+'C(occupation)[T.3.0]':'occ_3',
+'C(occupation)[T.4.0]':'occ_4',
+'C(occupation)[T.5.0]':'occ_5',
+'C(occupation)[T.6.0]':'occ_6',
+'C(occupation_husb)[T.2.0]':'occ_husb_2',
+'C(occupation_husb)[T.3.0]':'occ_husb_3',
+'C(occupation_husb)[T.4.0]':'occ_husb_4',
+'C(occupation_husb)[T.5.0]':'occ_husb_5',
+'C(occupation_husb)[T.6.0]':'occ_husb_6'})
+y = np.ravel(y)
+
+X.hist()
+# instantiate a logistic regression model, and fit with X and y
+model = LogisticRegression()
+model = model.fit(X, y)
+# check the accuracy on the training set
+print("Model score: ",model.score(X, y))
+
+print(" what percentage had affairs?")
+print(y.mean())
+C1=np.transpose(model.coef_)
+C=X.columns
+pd.DataFrame(C1,index=C ,columns={"Model Coeff.."})
